4cb8e835413fc73b8fbe5a454d074928d3a7a407
Shahbaj Hussain <shahbajhussain231@gmail.com>
2025-07-25 17:35:35 +0530

    modified

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,5 +1,1 @@
-<<<<<<< HEAD
 export const BASE_URL = import.meta.env.VITE_BACKEND_URL;
-=======
-export const BASE_URL="https://dev-tinder-7.onrender.com"
->>>>>>> 8d3b13ab1cf2195b46a63643f2580adea96d28ff
